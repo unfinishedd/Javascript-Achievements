cd0a743df3e764af7b05f15ce3d02842b233d31a
Sam <sam0000@live.nl>
2021-01-19 14:57:12 +0100

    git commit -m afgemaakt

---
+++ les6/To Do List/array.js
@@ -0,0 +1,41 @@
+//Create your arrays an variables
+//get DOM elements
+//
+
+btn_add.addEventListener("click", ()=>{
+    //handle adding a new todo
+
+});
+
+btn_finish.addEventListener("click", ()=>{
+    //handle finishing a todo
+    
+    //make sure your input is seen as a number
+    x = Number( );
+    //check if input really is a number
+    if(isNaN(x)){
+        //not a number
+    }else{
+        //is a number
+    }
+
+});
+
+btn_remove.addEventListener("click", ()=>{
+    //handle removing of a todo
+
+    //make sure your input is seen as a number
+    x = Number( );
+    //check if input really is a number
+    if(isNaN(x)){
+        //not a number
+    }else{
+        //is a number
+    }
+});
+
+function updateListing(){
+ //print list of todo's
+ //use loop to step trough the arrays
+   
+}
